47bdf9b7d576a40ecb33ca18f260b8f001a90193
student <ryankn@stud.ntnu.no>
2026-09-16 15:45:58 +0200

    day 2 complete

--- external_memory_driver.c
+++ external_memory_driver.c
@@ -1,7 +1,12 @@
 #include "external_memory_driver.h" 
 
+
 void external_memory_init(){
 
-    MCUCR |= (1<<SRE);
+    MCUCR |= (1<<SRE); //Enables external memory
+    SFIOR &= ~(111 << XMM0);
 
+    SFIOR |= (1<<XMM2); //Releases port pins PC7-PC4 from the SRAM
+    //EMCUCR|= (1<<SRW01);
+    
 }
--- main.c
+++ main.c
@@ -6,11 +6,13 @@
 
 #include "uart_driver.h"
 #include "external_memory_driver.h"
+#include "sram_test.h"
 
 #define WAVE_PIN PB1
 #define ERROR_LED PB0
 
-volatile uint8_t *adress = (volatile uint8_t *)0x1800;
+volatile uint8_t *address = (volatile uint8_t *)0x1800;
+volatile uint8_t *address_ADC = (volatile uint8_t *)0x1000;
 
 static FILE uartstdout = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 
@@ -19,17 +21,20 @@ int main(void) {
     /*
     set_bit(DDRB, WAVE_PIN);
 
+
+    //Day 1 initial test
     while(1) {
         set_bit(PORTB, WAVE_PIN);
         _delay_ms(500);
         clear_bit(PORTB, WAVE_PIN);
         _delay_ms(500);
     }*/
-/*
     uart_init();
     stdout = stdin = &uartstdout; // Replace the defualt stdout/in stream with the custom uart one
 
-    while(1) { 
+
+    //Day 1 test
+    /*while(1) { 
         
 
         //char c[32] = getchar(); // Waits until it gets a character on the stdin stream
@@ -41,12 +46,24 @@ int main(void) {
 
         
     external_memory_init();
-    adress[1]=1;
+    //SRAM_test(); //Day 2 SRAM test
+
+    //Day 2 final test
     while(1) {
+        //address[1]=0;
+        //_delay_ms(1000);
+        //address_ADC[2]=1;
+        //nal_delay_ms(1000);
+
         //for (int i=0; i<=10000; i++){
-        //adress[8]=1;
-        //_delay_ms(500);
-        }
+        //address[i]=1+i;
+        //_delay_ms(1000);
+       //}
+    }
+
+
+
+    
 
     return 0;
 }
